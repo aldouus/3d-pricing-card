437ae7b7a118a69963eda15b67d85070c3381099
Aldous <aldouswaites@gmail.com>
2024-05-12 21:27:23 +0200

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
+import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
 const mono = Roboto_Mono({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Banana Card",
+  description: "3D Pricing card using React Three Fiber",
+};
 
 export default function RootLayout({
   children,
