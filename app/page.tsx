e4ac3b76fe2d7ddde099b2b5cf8bc3fd54c399f8
Aldous <aldouswaites@gmail.com>
2024-05-12 21:04:08 +0200

    update path

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import PricingCard from "@/components/PricingCard";
-
-export default function Home() {
-  return (
-    <main className="h-screen grid place-content-center">
-      <PricingCard />
-    </main>
-  )
-}
